cff2a81911db850bf1576d95d2208302bc8d9eb4
evgeniy <vois.evgenii@gmail.com>
2020-03-08 15:26:52 +0300

    Start_of_project

---
+++ src/components/Header.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function Header() {
+  return (
+    <header className='header'>
+      <img
+        src='https://upload.wikimedia.org/wikipedia/commons/thumb/4/4a/Inkscape.logo.svg/390px-Inkscape.logo.svg.png'/>
+    </header>
+  );
+}
---
+++ src/components/Profile.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+export default function Profile() {
+  return (
+    <main className='content'>
+      <img
+        src='https://p.bigstockphoto.com/eIdTXLbqQilMs9xbjvcs_bigstock-Aerial-View-Of-Sandy-Beach-Wit-256330393.jpg'/>
+      <div className='bio'>
+        <img
+          class='ava'
+          src='https://miro.medium.com/max/1200/1*mk1-6aYaf_Bes1E3Imhc0A.jpeg'/>
+        <div>description</div>
+      </div>
+      <div className='posts'>
+        <div>Post 1</div>
+
+      </div>
+
+    </main>
+  );
+}
